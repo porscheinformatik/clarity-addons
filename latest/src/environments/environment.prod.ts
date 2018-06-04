c87b86048dd89d93a7ba23ea54babb00d8243119
Marcel Ring <marcel@ring-mail.at>
2018-06-04 14:23:28 +0200

    test build

--- latest/src/environments/environment.prod.ts
+++ latest/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   version: '0.1.0',
-  versions_url: '/versions.json',
+  versions_url: '/clarity-addons/versions.json',
   base: '/clarity-addons'
 };
