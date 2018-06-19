e1a79346e13661beba187a7328183310ed2dc8e5
Manuel Andres <Manuel.Andres@catalysts.cc>
2018-06-19 12:46:47 +0200

    Fix import paths of all modules

--- src/clr-addons/clr-addons.module.ts
+++ src/clr-addons/clr-addons.module.ts
@@ -6,9 +6,9 @@
 
 import { NgModule } from '@angular/core';
 
-import { ClrViewEditSectionModule } from './view-edit-section';
-import { ClrPagerModule } from './pager';
-import { ClrPagedSearchResultListModule } from './paged-search-result-list';
+import { ClrViewEditSectionModule } from './view-edit-section/view-edit-section.module';
+import { ClrPagerModule } from './pager/pager.module';
+import { ClrPagedSearchResultListModule } from './paged-search-result-list/paged-search-result-list.module';
 
 @NgModule({
   exports: [ClrViewEditSectionModule, ClrPagerModule, ClrPagedSearchResultListModule],
