9d956e6d8de843175d576002af72699d2e7d5eb0
Marcel Ring <marcel@ring-mail.at>
2018-06-20 11:49:48 +0200

    Add VU3 theme

--- src/dev/src/app/app.component.ts
+++ src/dev/src/app/app.component.ts
@@ -3,11 +3,28 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { Component } from '@angular/core';
+import { Component, PLATFORM_ID, Inject } from '@angular/core';
 import { Route } from '@angular/router';
 import { APP_ROUTES } from './app.routing';
+import { isPlatformBrowser, DOCUMENT } from '@angular/common';
 
 @Component({ selector: 'app-root', templateUrl: './app.component.html' })
 export class AppComponent {
   public routes: Route[] = APP_ROUTES;
+  linkRef: HTMLLinkElement;
+
+  themes = [{ name: 'VU3', href: '/styles/clr-addons-vu3.css' }, { name: 'MVAP', href: '/styles/clr-addons-mvap.css' }];
+
+  constructor(@Inject(DOCUMENT) private document: Document, @Inject(PLATFORM_ID) private platformId: Object) {
+    if (isPlatformBrowser(this.platformId)) {
+      this.linkRef = this.document.createElement('link');
+      this.linkRef.rel = 'stylesheet';
+      this.linkRef.href = this.themes[0].href;
+      this.document.querySelector('head').appendChild(this.linkRef);
+    }
+  }
+
+  setTheme(theme) {
+    this.linkRef.href = theme.href;
+  }
 }
