21d468afa868710c53f3cdb6b3da4971738abff2
Marcel Ring <marcel@ring-mail.at>
2018-07-02 09:18:42 +0200

    Adapt Fullpage layouts

--- latest/src/app/documentation/demos/full-page-layouts/sidebarpage-layout/sidebarpage-layout.ts
+++ latest/src/app/documentation/demos/full-page-layouts/sidebarpage-layout/sidebarpage-layout.ts
@@ -3,15 +3,35 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import {Component} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
     selector: "clr-sidebarpage-layout-demo",
     templateUrl: "./sidebarpage-layout.demo.html",
     styleUrls: ["./sidebarpage-layout.scss"]
 })
-export class SidebarpageLayoutDemo {
+export class SidebarpageLayoutDemo implements OnInit {
 
-    constructor() {
+    id: number;
+
+    constructor(private router: Router) {}
+
+    ngOnInit(): void {
+        this.id = this.collectRouteData("id")[0];
+    }
+
+    private collectRouteData(key: string) {
+        let route = this.router.routerState.snapshot.root;
+        let returnArray = [];
+
+        while (route) {
+            if (route.data && route.data[key]) {
+                returnArray.push(route.data[key]);
+            }
+            route = route.firstChild;
+        }
+
+        return returnArray;
     }
 }
