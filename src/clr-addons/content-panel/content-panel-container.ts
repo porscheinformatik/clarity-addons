e3549890940e5455628df5b2a6dc5cd50bb0919a
cat-mandres <40262875+cat-mandres@users.noreply.github.com>
2018-07-02 14:43:18 +0200

    Add clarity layout classes to content panel (#72)

--- src/clr-addons/content-panel/content-panel-container.ts
+++ src/clr-addons/content-panel/content-panel-container.ts
@@ -9,11 +9,12 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'clr-content-panel-container',
   template:
-    '<div class="content-panel-outer-content">' +
+    '<div class="content-area content-panel-outer-content">' +
     '<ng-content select=":not(clr-content-panel)"></ng-content>' +
     '</div>' +
     '<ng-content select="clr-content-panel"></ng-content>',
   host: {
+    '[class.content-container]': 'true',
     '[class.content-panel-container]': 'true',
   },
 })
