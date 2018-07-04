f4b74f3e5dd1275a33e1a963494c4e29ae1571c3
Marcel Ring <33118495+Wallabeng@users.noreply.github.com>
2018-07-04 15:09:52 +0200

    Add breadcrumb demo (#83)

---
+++ latest/src/app/documentation/demos/breadcrumb/breadcrumb.demo.module.ts
@@ -0,0 +1,33 @@
+/*
+ * Copyright (c) 2018 Porsche Informatik. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ClarityModule } from "@clr/angular";
+import { ClrAddonsModule } from "@porscheinformatik/clr-addons";
+
+import { BreadcrumbDemo } from "./breadcrumb.demo"
+import { DocWrapperModule } from "../_doc-wrapper/doc-wrapper.module";
+import { RouterModule } from "@angular/router";
+import { UtilsModule } from "../../../utils/utils.module";
+
+@NgModule({
+    imports: [
+        CommonModule,
+        UtilsModule,
+        DocWrapperModule,
+        RouterModule.forChild([{ path: "", component: BreadcrumbDemo }]),
+        ClarityModule,
+        ClrAddonsModule
+    ],
+    declarations: [
+        BreadcrumbDemo
+    ],
+    exports: [
+        BreadcrumbDemo
+    ]
+})
+export class BreadcrumbDemoModule {
+}
---
+++ latest/src/app/documentation/demos/breadcrumb/breadcrumb.demo.ts
@@ -0,0 +1,49 @@
+/*
+ * Copyright (c) 2018 Porsche Informatik. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import { Component, OnInit, Inject } from "@angular/core";
+import { ClarityDocComponent } from "../clarity-doc";
+import { ClrBreadcrumbService, ClrBreadcrumbModel } from "@porscheinformatik/clr-addons";
+
+const HTML_EXAMPLE = `
+<clr-breadcrumb></clr-breadcrumb>
+`
+
+const ANGULAR_EXAMPLE = `
+const breadcrumb1: ClrBreadcrumbModel = { label: "Home", url: "/" }
+const breadcrumb2: ClrBreadcrumbModel = { label: "Parent", url: "/" }
+const breadcrumb3: ClrBreadcrumbModel = { label: "Actual Page" }
+this.breadcrumbService.updateBreadcrumb([
+    breadcrumb1, breadcrumb2, breadcrumb3
+]);
+`
+
+@Component({
+    selector: "clr-breadcrumb-demo",
+    templateUrl: "./breadcrumb.demo.html",
+    host: {
+        "[class.content-area]": "true",
+        "[class.dox-content-panel]": "true"
+    },
+    styles: [ ".breadcrumb-demo > * { display: block; margin-top: -12px;" ,
+              ".clrweb-DoxMedia-block { min-height: 60px;}"]
+})
+export class BreadcrumbDemo extends ClarityDocComponent implements OnInit {
+    htmlExample = HTML_EXAMPLE;
+    angularExample = ANGULAR_EXAMPLE;
+
+    constructor(private breadcrumbService: ClrBreadcrumbService) {
+        super("breadcrumb");
+    }
+
+    ngOnInit() {
+        const breadcrumb1: ClrBreadcrumbModel = { label: "Home", url: "/" }
+        const breadcrumb2: ClrBreadcrumbModel = { label: "Parent", url: "/" }
+        const breadcrumb3: ClrBreadcrumbModel = { label: "Actual Page" }
+        this.breadcrumbService.updateBreadcrumb([
+            breadcrumb1, breadcrumb2, breadcrumb3
+        ]);
+    }
+}
