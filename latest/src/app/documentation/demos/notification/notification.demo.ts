da60eb08ffdc98086213ee3c27384b8754def350
Tetiana <tetiana@Ozzys-MacBook-Pro-2.local>
2018-07-27 09:41:24 +0300

    Added component usage.

--- latest/src/app/documentation/demos/notification/notification.demo.ts
+++ latest/src/app/documentation/demos/notification/notification.demo.ts
@@ -3,8 +3,9 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { Component } from "@angular/core";
+import { Component, ViewChildren, QueryList  } from "@angular/core";
 import { ClarityDocComponent } from "../clarity-doc";
+// import { ClrNotification, NotificationService } from '@porscheinformatik/clr-addons';
 
 const CODE_EXAMPLE = `
 <div class="btn-group">
@@ -34,9 +35,26 @@ const CODE_EXAMPLE = `
     }
 })
 export class NotificationDemo extends ClarityDocComponent {
+    // @ViewChildren(ClrNotification) clrNotifications: QueryList<ClrNotification>;
     codeExample = CODE_EXAMPLE;
 
+    // constructor(private notificationService: NotificationService) {
     constructor() {
         super("notification");
     }
+    // ngAfterViewInit() {
+    //     this.notificationService.elements = this.clrNotifications.toArray();
+    // }
+
+    // onClose(): void {
+    //     console.log('notification closed');
+    // }
+
+    // showAlert(): void {
+    //     alert('Button was clicked');
+    // }
+
+    // openNotify(id): void {
+    //     this.notificationService.openNotification(id);
+    // }
 }
