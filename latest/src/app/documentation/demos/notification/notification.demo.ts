9734412762840544a0357965bf7bdbef21931929
incora-dev <dev@incorainc.com>
2018-07-27 13:50:11 +0300

    Fix import ClrNotificationService

--- latest/src/app/documentation/demos/notification/notification.demo.ts
+++ latest/src/app/documentation/demos/notification/notification.demo.ts
@@ -5,7 +5,7 @@
  */
 import { Component, ViewChildren, QueryList } from "@angular/core";
 import { ClarityDocComponent } from "../clarity-doc";
-import { ClrNotification, NotificationService } from '@porscheinformatik/clr-addons';
+import { ClrNotification, ClrNotificationService } from '@porscheinformatik/clr-addons';
 
 const CODE_EXAMPLE = `
 <div class="btn-group">
@@ -32,17 +32,18 @@ const CODE_EXAMPLE = `
     host: {
         "[class.content-area]": "true",
         "[class.dox-content-panel]": "true"
-    }
+    },
+    providers: [ClrNotificationService]
 })
 export class NotificationDemo extends ClarityDocComponent {
     @ViewChildren(ClrNotification) clrNotifications: QueryList<ClrNotification>;
     codeExample = CODE_EXAMPLE;
 
-    constructor(private notificationService: NotificationService) {
+    constructor(private notificationService: ClrNotificationService) {
         super("notification");
     }
     ngAfterViewInit() {
-        this.notificationService.elements = this.clrNotifications.toArray();
+        this.notificationService.setElements(this.clrNotifications.toArray());
     }
 
     onClose(): void {
