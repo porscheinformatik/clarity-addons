11b32aa6130eba3d44220669673834ec1d830e3d
Manuel Andres <Manuel.Andres@catalysts.cc>
2018-08-02 14:43:36 +0200

    Update flowbar layout demo with sub step logic inside component
    - card demo updated
    - paged search result list demo updated
    - clr-addons version updated

--- latest/src/app/documentation/demos/full-page-layouts/flow-bar-layout/flow-bar-layout.demo.ts
+++ latest/src/app/documentation/demos/full-page-layouts/flow-bar-layout/flow-bar-layout.demo.ts
@@ -6,10 +6,6 @@
 import { Component, ViewChild } from "@angular/core";
 import { ClrFlowBar, ClrFlowBarStep } from "@porscheinformatik/clr-addons";
 
-interface FlowBarDemoStep extends ClrFlowBarStep {
-    subSteps: ClrFlowBarStep[];
-}
-
 @Component({
     selector: "clr-flow-bar-layout-demo",
     templateUrl: "./flow-bar-layout.demo.html"
@@ -17,8 +13,8 @@ interface FlowBarDemoStep extends ClrFlowBarStep {
 export class FlowBarLayoutDemo {
     @ViewChild("flowBar") flowBar: ClrFlowBar;
 
-    activeStep: FlowBarDemoStep;
-    flowBarSteps: FlowBarDemoStep[] = [
+    activeStep: ClrFlowBarStep;
+    flowBarSteps: ClrFlowBarStep[] = [
         {
             title: "Step", enabled: true, subSteps: [
                 { title: "Sub Step 1/3", enabled: true },
@@ -32,63 +28,20 @@ export class FlowBarLayoutDemo {
                 { title: "Sub Step 2/2", enabled: true }
             ]
         },
-        { title: "Step", enabled: true, subSteps: [] },
-        { title: "Step", enabled: true, subSteps: [] }
+        { title: "Step", enabled: true },
+        {
+            title: "Step", enabled: true, subSteps: [
+                { title: "Sub Step 1/2", enabled: true },
+                { title: "Sub Step 2/2", enabled: true }
+            ]
+        }
     ];
-    activeSubStep: ClrFlowBarStep;
 
-    public setActiveStep(step: FlowBarDemoStep) {
-        const previousIndex = this.flowBarSteps.indexOf(this.activeStep);
+    public setActiveStep(step: ClrFlowBarStep) {
         this.activeStep = step;
-        if (this.activeStep.subSteps.length > 0) {
-            if (previousIndex < this.flowBarSteps.indexOf(this.activeStep)) {
-                this.activeSubStep = this.activeStep.subSteps[0];
-            } else {
-                this.activeSubStep = this.activeStep.subSteps[this.activeStep.subSteps.length - 1];
-            }
-        }
     }
 
-    public openPreviousStep(): void {
-        // If active step has no sub steps or first sub step is selected -> go to previous flow bar step
-        if (!this.activeStep.subSteps || this.activeStep.subSteps.length === 0
-            || this.activeSubStep === this.activeStep.subSteps[0]) {
-            this.flowBar.previous();
-        } else {
-            const subStepIndex = this.activeStep.subSteps.indexOf(this.activeSubStep);
-            this.activeSubStep = this.activeStep.subSteps[subStepIndex - 1];
-        }
-    }
-
-    public openNextStep(): void {
-        // If active step has no sub steps or last sub step is selected -> go to next flow bar step
-        if (!this.activeStep.subSteps || this.activeStep.subSteps.length === 0
-            || this.activeSubStep === this.activeStep.subSteps[this.activeStep.subSteps.length - 1]) {
-            this.flowBar.next();
-        } else {
-            const subStepIndex = this.activeStep.subSteps.indexOf(this.activeSubStep);
-            this.activeSubStep = this.activeStep.subSteps[subStepIndex + 1];
-        }
-    }
-
-    public isPreviousAvailable(): boolean {
-        if (this.activeStep) {
-            if (!this.activeStep.subSteps || this.activeStep.subSteps.length === 0
-                || this.activeSubStep === this.activeStep.subSteps[0]) {
-                return this.flowBar.isPreviousAvailable();
-            } else {
-                return true;
-            }
-        }
-    }
-
-    public isNextAvailable(): boolean {
-        if (this.activeStep) {
-            if (!this.activeStep.subSteps || this.activeStep.subSteps.length === 0) {
-                return this.flowBar.isNextAvailable();
-            } else {
-                return this.activeSubStep !== this.activeStep.subSteps[0];
-            }
-        }
+    public setActiveSubStep(step: ClrFlowBarStep) {
+        this.flowBar.changeActiveSubStep(step);
     }
 }
