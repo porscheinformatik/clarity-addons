73901f343829a33f3cfa36c5fc4a9d296f971ae4
Marcel Ring <33118495+Wallabeng@users.noreply.github.com>
2018-09-27 13:16:58 +0200

    Adapt View/Edit-section to new forms (#229)

--- src/clr-addons/view-edit-section/view-edit-section.module.ts
+++ src/clr-addons/view-edit-section/view-edit-section.module.ts
@@ -7,12 +7,12 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { ClarityModule } from '@clr/angular';
+import { ClarityModule, ClrFormsNextModule } from '@clr/angular';
 
 import { ClrViewEditSection } from './view-edit-section';
 
 @NgModule({
-  imports: [CommonModule, ClarityModule, FormsModule],
+  imports: [CommonModule, ClarityModule, FormsModule, ClrFormsNextModule],
   declarations: [ClrViewEditSection],
   exports: [ClrViewEditSection],
 })
