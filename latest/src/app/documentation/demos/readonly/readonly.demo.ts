1b093cf332f957bd3ca92726ecb3dc0fe97dad93
Marcel Ring <marcel@ring-mail.at>
2018-12-11 13:55:56 +0100

    Add readonly demo

---
+++ latest/src/app/documentation/demos/readonly/readonly.demo.ts
@@ -0,0 +1,50 @@
+/*
+ * Copyright (c) 2018 Porsche Informatik. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+import {Component} from "@angular/core";
+import {ClarityDocComponent} from "../clarity-doc";
+
+const HTML_CHECKBOX_EXAMPLE = `
+<clr-checkbox-container readonly>
+    ...
+</clr-checkbox-container>
+
+<clr-checkbox-container readonly show-only-selected="true">
+    ...
+</clr-checkbox-container>
+`;
+
+const HTML_RADIOBUTTON_EXAMPLE = `
+<clr-radiobutton-container readonly>
+    ...
+</clr-radiobutton-container>
+
+<clr-radiobutton-container readonly show-only-selected="true">
+    ...
+</clr-radiobutton-container>
+`;
+
+
+@Component({
+    selector: "clr-readonly-demo",
+    templateUrl: "./readonly.demo.html",
+    host: {
+        "[class.content-area]": "true",
+        "[class.dox-content-panel]": "true"
+    }
+})
+export class ReadonlyDemo extends ClarityDocComponent {
+    htmlCheckboxExample = HTML_CHECKBOX_EXAMPLE;
+    htmlRadiobuttonExample = HTML_RADIOBUTTON_EXAMPLE;
+
+    radioValue: number = 1;
+    checkValue1: boolean = true;
+    checkValue2: boolean;
+    checkValue3: boolean = true;
+
+    constructor() {
+        super("readonly");
+    }
+}
