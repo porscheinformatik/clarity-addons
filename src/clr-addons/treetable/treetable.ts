730aa8dd1d3839bebe9c503334e4e1ccc7a5db6b
Roxana Pop <Roxana.Pop@catalysts.cc>
2019-01-30 15:28:03 +0200

    fix clickable rows bug
    Signed-off-by: Roxana Pop <Roxana.Pop@catalysts.cc>

--- src/clr-addons/treetable/treetable.ts
+++ src/clr-addons/treetable/treetable.ts
@@ -3,7 +3,7 @@
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
-import { AfterContentInit, Component, ContentChildren, Input, OnChanges, QueryList } from '@angular/core';
+import { Component, ContentChildren, Input, QueryList } from '@angular/core';
 import { ClrTreetableColumn } from './treetable-column';
 import { ClrTreetableRow } from './treetable-row';
 
@@ -12,35 +12,33 @@ import { ClrTreetableRow } from './treetable-row';
   templateUrl: './treetable.html',
   host: { '[class.empty]': 'empty', '[class.treetable-host]': 'true' },
 })
-export class ClrTreetable implements AfterContentInit, OnChanges {
+export class ClrTreetable {
   @Input() clrClickableRows = true;
   @Input('clrHideHeader') hideHeader = false;
 
-  @ContentChildren(ClrTreetableRow, { descendants: true })
-  ttRows: QueryList<ClrTreetableRow>;
   @ContentChildren(ClrTreetableColumn, { descendants: true })
   ttColumns: QueryList<ClrTreetableRow>;
 
   empty = true;
 
-  ngAfterContentInit(): void {
+  private _ttRows: QueryList<ClrTreetableRow>;
+
+  @ContentChildren(ClrTreetableRow, { descendants: true })
+  set ttRows(items) {
+    this._ttRows = items;
     this.initClickableRows();
     this.initEmpty();
   }
 
-  ngOnChanges(): void {
-    this.initClickableRows();
-  }
-
   private initClickableRows(): void {
-    if (this.ttRows) {
-      this.ttRows.forEach((ttRow, index) => {
+    if (this._ttRows) {
+      this._ttRows.forEach((ttRow, index) => {
         ttRow.clickable = this.clrClickableRows;
       });
     }
   }
 
   private initEmpty(): void {
-    this.empty = this.ttRows.length === 0;
+    this.empty = this._ttRows.length === 0;
   }
 }
