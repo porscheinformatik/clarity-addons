925ff824ec626fcb6fd68029440f494c6074d775
cat-mandres <40262875+cat-mandres@users.noreply.github.com>
2019-02-15 09:52:58 +0100

    Add index ts file for date-time-container (#348)
    version 5.1.1

---
+++ src/clr-addons/date-time-container/index.ts
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2018 Porsche Informatik. All Rights Reserved.
+ * This software is released under MIT license.
+ * The full license information can be found in LICENSE in the root directory of this project.
+ */
+
+export * from './date-time-container';
+export * from './date-time-module';
+export * from './time-input';
