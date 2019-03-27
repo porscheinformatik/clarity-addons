4499fc8fea249f408f9799f380aa72ea173c2683
Anirudh Gaur <Anirudh.Gaur@catalysts.cc>
2019-03-27 10:09:11 +0100

    Usage hints for Server driven datagrid

--- latest/src/app/documentation/demos/datagrid/datagrid.demo.ts
+++ latest/src/app/documentation/demos/datagrid/datagrid.demo.ts
@@ -16,6 +16,61 @@ const NO_SELECT_ALL_HTML = `
     <clr-dg-row clrDgItem="5"><clr-dg-cell>Item 5</clr-dg-cell></clr-dg-row>
 </clr-datagrid>`
 
+const FIX_DATAGRID_HEIGHT = `
+.datagrid-host {
+  height: 478px;
+}
+.datagrid {
+  height: 470px;
+}`
+
+const SERVER_DRIVEN_DATAGRID_HTML = `
+<clr-datagrid id="activity-datagrid" (clrDgRefresh)="onRefresh($event)" [(clrDgSelected)]="selected">
+.....
+<clr-dg-footer>
+    <clr-dg-column-toggle>
+      <clr-dg-column-toggle-title>{{"tasks.activityDatagrid.showHide" | translate}}</clr-dg-column-toggle-title>
+      <clr-dg-column-toggle-button>{{"tasks.activityDatagrid.selectAll" | translate}}</clr-dg-column-toggle-button>
+    </clr-dg-column-toggle>
+    <clr-dg-pagination #pagination [clrDgTotalItems]="activityResults.hitCount">
+      {{pagination.firstItem + 1}} - {{pagination.lastItem + 1}}
+      of {{activityResults.hitCount}} {{"tasks.activityDatagrid.paging.title" | translate}}
+    </clr-dg-pagination>
+  </clr-dg-footer>
+</clr-datagrid>`
+
+const SERVER_DRIVEN_DATAGRID_TS = `
+import { ClrDatagridSortOrder, ClrDatagridStateInterface } from '@clr/angular';
+.....
+private onRefresh(state: ClrDatagridStateInterface): void {
+    this.loading = true;
+    const filters: { [prop: string]: any[] } = {};
+    if (state.filters) {
+      const statusFilter = state.filters[0] as ActivityListStatusFilterComponent;
+      if (!!statusFilter.getActivityStatusFilter().handled && !statusFilter.getActivityStatusFilter().open) {
+        this.searchParams.handled = ActivitySearchParametersPDTO.HandledEnum.HANDLED;
+      }
+    }
+    if (state.sort) {
+      this.searchParams.sortColumn = state.sort.by as SortColumnEnum;
+      if (!!state.sort.reverse) {
+        this.searchParams.sortDirection = ActivitySearchParametersPDTO.SortDirectionEnum.DESC;
+      } else {
+        this.searchParams.sortDirection = ActivitySearchParametersPDTO.SortDirectionEnum.ASC;
+      }
+    }
+    if (!!state.page) {
+      this.searchParams.offset = state.page.from;
+    }
+    this.activityListService.getActivities(this.searchParams).subscribe(result => {
+      this.activityResults = result;
+      this.loading = false;
+    }, catchError(error => {
+      this.loading = false;
+      return throwError(error);
+    }));
+  }`
+
 @Component({
     selector: "clr-datagrid-demo-docu",
     templateUrl: "./datagrid.demo.html",
@@ -26,6 +81,9 @@ const NO_SELECT_ALL_HTML = `
 })
 export class DatagridDemo extends ClarityDocComponent {
     noSelectAllExample = NO_SELECT_ALL_HTML;
+    fixDatagridHeight = FIX_DATAGRID_HEIGHT;
+    serverDrivenDatagridHtml = SERVER_DRIVEN_DATAGRID_HTML;
+    serverDrivenDatagridTs = SERVER_DRIVEN_DATAGRID_TS;
     selected: any[] = [];
 
     constructor() {
