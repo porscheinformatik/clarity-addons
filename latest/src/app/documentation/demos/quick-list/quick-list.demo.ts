24a6a0ed2819614b1198e5deeef9295516105461
ziedom12 <zied0omrane@gmail.com>
2019-04-30 15:27:16 +0200

    Quick List, improve documentation (#415)

--- latest/src/app/documentation/demos/quick-list/quick-list.demo.ts
+++ latest/src/app/documentation/demos/quick-list/quick-list.demo.ts
@@ -2,15 +2,42 @@ import { Component, OnInit } from '@angular/core';
 import {ClarityDocComponent} from "../clarity-doc";
 import {ClrQuickListValue} from "@porscheinformatik/clr-addons";
 
-const HTML_EXAMPLE = `
-<clr-quick-list class="clr-col-4"
-                [clrAllValues]="possibleOptions" 
-                [(clrValues)]="selectedOptions"
-                [clrAddLabel]="ADD OPTIONS"
-                [clrBlankOption]="BLANK_OPTION" 
-                [clrMandatory]="true">
-</clr-quick-list>
+const HTML_EXAMPLE1 = `
+<div class="clr-col-3">
+    <clr-quick-list [clrAllValues]="possibleOptions" 
+                    [(clrValues)]="selectedOptions"
+                    [clrAddLabel]="ADD OPTION"
+                    [clrBlankOption]="BLANK_OPTION" 
+                    [clrMandatory]="true">
+    </clr-quick-list>
+</div>
 `;
+
+const HTML_EXAMPLE2 = `
+BLANK_OPTION: ClrQuickListValue<string> = { id: '-BLANK-', label: '- Select -', value: null };
+selectedOptions: Array<ClrQuickListValue<string>> = [];
+possibleOptions: Array<ClrQuickListValue<string>> = [];
+options: Array<string> = [
+  'First option',
+  'Second option',
+  'Third Option',
+  'Fourth option',
+  'Fifth option'
+];
+
+ngOnInit() {
+  this.selectedOptions = [this.BLANK_OPTION];
+  this.possibleOptions = this.options.map(op => this.map(op));
+}
+
+private map(op: string): ClrQuickListValue<string> {
+  return new class implements ClrQuickListValue<string> {
+    id = op.substr(0, 2);
+    label = op;
+    value = op;
+  }();
+}
+`
 
 @Component({
   selector: 'app-quick-list',
@@ -21,8 +48,9 @@ const HTML_EXAMPLE = `
     }
 })
 export class QuickListDemo extends ClarityDocComponent implements OnInit {
-  htmlExample = HTML_EXAMPLE;
-  BLANK_OPTION: ClrQuickListValue<string> = { id: '-BLANK-', label: '- Select an option -', value: null };
+    htmlExample1 = HTML_EXAMPLE1;
+    htmlExample2 = HTML_EXAMPLE2;
+  BLANK_OPTION: ClrQuickListValue<string> = { id: '-BLANK-', label: '- Select -', value: null };
   selectedOptions: Array<ClrQuickListValue<string>> = [];
   possibleOptions: Array<ClrQuickListValue<string>> = [];
   options: Array<string> = [
@@ -42,14 +70,14 @@ export class QuickListDemo extends ClarityDocComponent implements OnInit {
   }
 
   private map(op: string): ClrQuickListValue<string> {
-        return new class implements ClrQuickListValue<string> {
-            id = op.substr(0, 2);
-            label = op;
-            value = op;
-        }();
-    }
+    return new class implements ClrQuickListValue<string> {
+        id = op.substr(0, 2);
+        label = op;
+        value = op;
+    }();
+  }
 
-    getAddLabel(): string {
-        return 'ADD OPTION';
-    }
+  getAddLabel(): string {
+      return 'ADD OPTION';
+  }
 }
