eb89be6651c4f81f3171bdb09b6c6fcf24ad68cb
Marcel Ring <marcel@ring-mail.at>
2019-05-06 10:13:29 +0200

    fix

--- src/clr-addons/letter-avatar/letter-avatar.ts
+++ src/clr-addons/letter-avatar/letter-avatar.ts
@@ -4,7 +4,7 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
-import { Component, OnInit, Input, HostBinding, ViewChild, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
+import { Component, Input, HostBinding, ViewChild, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'clr-letter-avatar',
