b37f734e2760fa0dca24fabba32f96085ba9f2eb
Marcel Ring <marcel@ring-mail.at>
2019-08-07 07:02:42 +0200

    Fix combobox
    + preselected option gets selected when options change
    + enter reopened combobox, as the search got triggered
    + ExpressionChangedAfterItHasBeenCheckedError for selected option

--- src/dev/src/app/combobox/combobox.demo.ts
+++ src/dev/src/app/combobox/combobox.demo.ts
@@ -4,6 +4,10 @@
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 import { Component } from '@angular/core';
+import { of } from 'rxjs';
+import { delay } from 'rxjs/operators';
 
 @Component({ templateUrl: './combobox.demo.html' })
-export class ComboboxDemo {}
+export class ComboboxDemo {
+  values$ = of(['Option 4', 'Option 5', 'Option 6']).pipe(delay(500));
+}
