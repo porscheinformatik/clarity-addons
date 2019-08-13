10203d9c74b490b1836925f1f229a72234883a89
Adrian Chioran <Adrian.Chioran@catalysts.cc>
2019-08-13 14:51:44 +0300

    fix demo page display

--- src/dev/src/app/quick-list/quick-list.demo.module.ts
+++ src/dev/src/app/quick-list/quick-list.demo.module.ts
@@ -7,9 +7,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { QuickListDemo } from './quick-list.demo';
 import { ClrAddonsModule } from '@porscheinformatik/clr-addons';
+import { ClrInputModule } from '@clr/angular';
 
 @NgModule({
-  imports: [CommonModule, ClrAddonsModule],
+  imports: [CommonModule, ClrAddonsModule, ClrInputModule],
   declarations: [QuickListDemo],
   exports: [QuickListDemo],
 })
