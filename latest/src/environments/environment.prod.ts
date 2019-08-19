450d126819e67c3b2d0476d5072e8c05bc49c367
Marcel Ring <marcel@ring-mail.at>
2019-08-19 09:27:58 +0200

    Use version 6.2.0

--- latest/src/environments/environment.prod.ts
+++ latest/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   version: 'latest',
-  version_display: '6.1.3',
+  version_display: '6.2.0',
   versions_url: '/clarity-addons/versions.json',
   base: '/clarity-addons'
 };
