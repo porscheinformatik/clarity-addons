bbcdf35d7339bb8ec994b7a73ff538968e0b3c99
Marcel Ring <marcel@ring-mail.at>
2019-08-21 11:04:49 +0200

    Demo: Fixes for forms and notification

--- latest/src/environments/environment.prod.ts
+++ latest/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   version: 'latest',
-  version_display: '6.2.1',
+  version_display: '6.2.2',
   versions_url: '/clarity-addons/versions.json',
   base: '/clarity-addons'
 };
