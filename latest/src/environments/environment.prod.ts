e5a6e4593e22ab82a7027cd349f7c8a0648b2fc5
Christian Köberl <christian.koeberl@porscheinformatik.at>
2019-10-23 10:21:29 +0200

    Update version in environment

--- latest/src/environments/environment.prod.ts
+++ latest/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   version: 'latest',
-  version_display: '6.3.5',
+  version_display: '6.4.0',
   versions_url: '/clarity-addons/versions.json',
   base: '/clarity-addons'
 };
