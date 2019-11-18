4cd479b2b177ae44cf7f730b5df88d277372e640
Marcel Ring <33118495+Wallabeng@users.noreply.github.com>
2019-11-18 16:11:56 +0100

    Use version 6.4.2 (#571)

--- latest/src/environments/environment.prod.ts
+++ latest/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   version: "latest",
-  version_display: "6.4.1",
+  version_display: "6.4.2",
   versions_url: "/clarity-addons/versions.json",
   base: "/clarity-addons"
 };
