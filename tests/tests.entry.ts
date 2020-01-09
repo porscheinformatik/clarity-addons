557287bae01bf923775063ca4e2404877424546e
Marcel Ring <marcel@ring-mail.at>
2020-01-09 07:30:58 +0100

    Update test dependencies

--- tests/tests.entry.ts
+++ tests/tests.entry.ts
@@ -1,12 +1,11 @@
 /*
- * Copyright (c) 2018-2019 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2020 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
 import 'zone.js/dist/zone';
 import 'zone.js/dist/zone-testing';
-import 'jasmine-expect';
 import { getTestBed } from '@angular/core/testing';
 import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
 
