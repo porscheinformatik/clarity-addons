6bae048866bf4895a7a65946707e5adcb90d560c
Christian Köberl <derkoe@localhost.localdomain>
2020-03-03 12:20:09 +0100

    fix: @Input cannot be used with @ContentChild

--- src/clr-addons/paged-search-result-list/paged-search-result-list.ts
+++ src/clr-addons/paged-search-result-list/paged-search-result-list.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2020 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -34,7 +34,6 @@ export class ClrPagedSearchResultList {
   /**
    * The template how each item should be displayed.
    */
-  @Input('clrItemTemplate')
   @ContentChild(TemplateRef, { static: true })
   itemTemplate: TemplateRef<any>;
 
