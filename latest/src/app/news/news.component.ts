2c6a1497826fca540a3a320b25e5ee8f70f00508
Wallabeng <marcel@ring-mail.at>
2020-04-04 12:52:58 +0200

    Remove news section

--- latest/src/app/news/news.component.ts
+++ latest/src/app/news/news.component.ts
@@ -1,8 +1,4 @@
-import {
-    Component, OnDestroy, ElementRef,
-} from "@angular/core";
-import { Router, NavigationEnd } from "@angular/router";
-import { Subscription } from "rxjs";
+import { Component } from "@angular/core";
 
 @Component({
     selector: "news",
@@ -11,28 +7,5 @@ import { Subscription } from "rxjs";
         "[class.content-container]": "true"
     }
 })
-export class NewsComponent implements OnDestroy {
-    releaseNumber: string;
-    hasBreakingChange: boolean;
-
-    private _subscriptions: Subscription[] = [];
-
-    constructor(private router: Router, private el: ElementRef) {
-        this._subscriptions.push(this.router.events.subscribe((change: any) => {
-            if (change instanceof NavigationEnd) {
-                let url: string[] = change.url.split("/");
-                let urlLength: number = url.length;
-                setTimeout(() => {
-                    if (urlLength > 0 && url[urlLength - 1] !== "news") {
-                        this.releaseNumber = url[urlLength - 1];
-                    }
-                    this.hasBreakingChange = this.el.nativeElement.querySelector(".breaking-change") !== null;
-                }, 0);
-            }
-        }));
-    }
-
-    ngOnDestroy() {
-        this._subscriptions.forEach((sub: Subscription) => sub.unsubscribe());
-    }
+export class NewsComponent {
 }
