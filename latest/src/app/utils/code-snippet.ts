9d25d602c8b61ca2caee4d255dc31e616e7ad0be
Manuel Andres <Manuel.Andres@cloudflight.io>
2020-04-06 11:18:00 +0200

    Update to angular 9

--- latest/src/app/utils/code-snippet.ts
+++ latest/src/app/utils/code-snippet.ts
@@ -24,7 +24,7 @@ import {CodeHighlight} from "./code-highlight";
     `]
 })
 export class CodeSnippet implements AfterViewInit {
-    @ViewChild(CodeHighlight, { static: false }) codeHighlight: CodeHighlight;
+    @ViewChild(CodeHighlight) codeHighlight: CodeHighlight;
 
     @Input("clrCode") public code: string;
     @Input("clrLanguage") public language: string = "html";
--- latest/src/app/utils/scrollspy.directive.ts
+++ latest/src/app/utils/scrollspy.directive.ts
@@ -2,7 +2,7 @@
  * Hack while waiting for https://github.com/angular/angular/issues/6595 to be fixed.
  */
 
-import {Directive, Input, OnDestroy, OnInit, ContentChildren, QueryList, Renderer, ElementRef} from "@angular/core";
+import { Directive, Input, OnDestroy, OnInit, ContentChildren, QueryList, ElementRef, Renderer2 } from "@angular/core";
 import {RouterLinkWithHref} from "@angular/router";
 import {Subscription} from "rxjs";
 
@@ -11,7 +11,7 @@ import {Subscription} from "rxjs";
 })
 export class ScrollSpy implements OnDestroy, OnInit {
 
-    constructor(private renderer: Renderer) {
+    constructor(private renderer: Renderer2) {
     }
 
     @Input("scrollspy") scrollable: any;
@@ -40,7 +40,7 @@ export class ScrollSpy implements OnDestroy, OnInit {
             window.requestAnimationFrame(() => {
                 let currentLinkIndex = this.findCurrentAnchor() || 0;
                 this.linkElements.forEach((link: ElementRef, index: number) => {
-                    this.renderer.setElementClass(link.nativeElement, "active", index === currentLinkIndex);
+                    index === currentLinkIndex ? this.renderer.addClass(link.nativeElement, "active") : this.renderer.removeClass(link.nativeElement, "active");
                 });
                 this.throttle = false;
             });
