b43bfa06cf5f464cf62c7206d44e7bf81219963d
Marcel Ring (windm) <extern.marcel.winder@porscheinformatik.at>
2020-04-16 10:23:00 +0200

    Update dependency prettier and @types/node

--- src/clr-addons/content-panel/content-panel-container-content.ts
+++ src/clr-addons/content-panel/content-panel-container-content.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2020 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -8,9 +8,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'clr-content-panel-container-content',
-  template: `
-    <ng-content></ng-content>
-  `,
+  template: ` <ng-content></ng-content> `,
   host: {
     '[class.content-area]': 'true',
   },
