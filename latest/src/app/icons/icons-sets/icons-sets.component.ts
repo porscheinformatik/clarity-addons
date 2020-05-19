96d65d426f43214195229b81402ba9bd0bf2fcf0
Manuel Andres <Manuel.Andres@cloudflight.io>
2020-05-19 17:37:49 +0200

    Add legal disclaimer, add more icons

--- latest/src/app/icons/icons-sets/icons-sets.component.ts
+++ latest/src/app/icons/icons-sets/icons-sets.component.ts
@@ -1,16 +1,10 @@
 import { Component } from "@angular/core";
+import { ClrAddonsIconShapes } from "@porscheinformatik/clr-addons";
 
 @Component({
     selector: "icons-sets",
     templateUrl: "./icons-sets.component.html"
 })
 export class IconsSetsComponent {
-    // A list of all shapes we want to display
-    // TODO replace with list of ClrAddon Icons once it is exported correctly
-    shapes: string[] = [
-        'new-car-commercial',
-        'new-car-private',
-        'used-car-commercial',
-        'used-car-private'
-    ];
+    shapes: string[] = Object.keys(ClrAddonsIconShapes);
 }
--- latest/src/app/icons/icons.component.ts
+++ latest/src/app/icons/icons.component.ts
@@ -1,11 +1,10 @@
 import { Component } from "@angular/core";
-import { EssentialShapes } from "@clr/icons/shapes/essential-shapes";
+import { ClrAddonsIconShapes } from "@porscheinformatik/clr-addons";
 
 @Component({
     selector: "icons",
     templateUrl: "icons.component.html"
 })
 export class IconsComponent {
-// TODO replace with Clr Addons Shapes instead
-    backgroundIcons: string[] = Object.keys(EssentialShapes);
+    backgroundIcons: string[] = Object.keys(ClrAddonsIconShapes);
 }
