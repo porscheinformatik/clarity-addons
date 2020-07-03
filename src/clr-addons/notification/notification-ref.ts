f7c6afba85603e7c26316f0ac88e2f4b92638b06
Manuel Andres <Manuel.Andres@cloudflight.io>
2020-07-03 14:21:16 +0200

    Add possibility to use variables in Clr Notification when displaying it as ng-template
    + Increase to version 8.0.3

--- src/clr-addons/notification/notification-ref.ts
+++ src/clr-addons/notification/notification-ref.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2020 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -9,13 +9,15 @@ import { ClrNotification } from './notification';
 import { take } from 'rxjs/operators';
 
 /*
- * Copyright (c) 2018-2019 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2020 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
 
 /**
  * A reference to an active (currently opened) notification.
+ *
+ * @deprecated not used anymore.
  */
 export class ClrActiveNotification {}
 
