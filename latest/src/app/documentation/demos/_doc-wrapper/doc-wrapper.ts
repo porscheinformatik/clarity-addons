3ca78e6bf4cec616044cc327631ee6798c3086a6
Marcel Ring <33118495+Wallabeng@users.noreply.github.com>
2021-01-12 12:29:36 +0100

    Cleanup unused stuff (#966)
    Co-authored-by: Marcel Ring (windm) <extern.marcel.winder@porscheinformatik.at>

--- latest/src/app/documentation/demos/_doc-wrapper/doc-wrapper.ts
+++ latest/src/app/documentation/demos/_doc-wrapper/doc-wrapper.ts
@@ -14,29 +14,6 @@ import {Component, Input} from "@angular/core";
 })
 export class DocWrapper {
     @Input() title: string = "";
-    @Input() newLayout: boolean = false;
-    @Input() ui: number = 0;
-    @Input() ng: number = 0;
     @Input() description: string = "";
     @Input() deprecated = false;
-
-    get useNewLayout() {
-        return !!this.newLayout;
-    }
-
-    get uiInProgress() {
-        return 4 < this.ui && this.ui < 20;
-    }
-
-    get uiDone() {
-        return this.ui > 19;
-    }
-
-    get ngInProgress() {
-        return 4 < this.ng && this.ui < 20;
-    }
-
-    get ngDone() {
-        return this.ng > 19;
-    }
 }
--- latest/src/app/documentation/demos/clarity-doc.ts
+++ latest/src/app/documentation/demos/clarity-doc.ts
@@ -1,20 +1,14 @@
 import {COMPONENT_MAP} from "../../utils/component-list";
 
 export class ClarityDocComponent {
-    ui: number = -1;
-    ng: number = -1;
     title: string = "";
-    newLayout: boolean = false;
 
     constructor(componentName: string) {
         let component = COMPONENT_MAP.get(componentName);
-        this.populateComponentDetails(component.text, component.ui, component.ng, component.newLayout);
+        this.populateComponentDetails(component.text);
     }
 
-    populateComponentDetails(title: string, ui: number, ng: number, newLayout: boolean) {
-        this.ui = ui;
-        this.ng = ng;
+    populateComponentDetails(title: string) {
         this.title = title;
-        this.newLayout = newLayout;
     }
 }
