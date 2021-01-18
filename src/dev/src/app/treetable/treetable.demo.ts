c2d8efde07479934d8defd4ecae51fd3bb2c0c6a
Marcel Ring (windm) <extern.marcel.winder@porscheinformatik.at>
2021-01-18 09:34:28 +0100

    Treetable: Fix EHCAIHBC error for clrExpandable input

--- src/dev/src/app/treetable/treetable.demo.ts
+++ src/dev/src/app/treetable/treetable.demo.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2019 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2021 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -10,4 +10,21 @@ import { Component } from '@angular/core';
   selector: 'treetable-demo',
   templateUrl: './treetable.demo.html',
 })
-export class TreetableDemo {}
+export class TreetableDemo {
+  root = [
+    {
+      name: 'Parent1',
+      type: 'Parent',
+      version: 'V1',
+      children: [
+        {
+          name: 'Child1',
+          type: 'Child',
+          version: 'V1',
+          children: [{ name: 'ChildChild1', type: 'ChildChild', version: 'V1' }],
+        },
+        { name: 'Child2', type: 'Child', version: 'V1' },
+      ],
+    },
+  ];
+}
