591e7b8ac330ee2d3a82e157b8a8d53fac25341c
Marcel Ring <33118495+Wallabeng@users.noreply.github.com>
2021-04-27 10:00:04 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
-    'prettier/@typescript-eslint',
+    'prettier',
   ],
   parser: '@typescript-eslint/parser',
   parserOptions: {
