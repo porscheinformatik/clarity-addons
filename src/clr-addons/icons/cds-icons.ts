4ea79dd8fb58f8629bc81151bd8af02ccd2414db
Szilard Veress <Szilard.Veress@cloudflight.io>
2021-07-15 13:25:44 +0300

    Upgrade version

---
+++ src/clr-addons/icons/cds-icons.ts
@@ -0,0 +1,6 @@
+import { ClarityIcons } from '@cds/core/icon';
+import { timelineIcon } from '@cds/core/icon/shapes/timeline';
+
+export function initCdsIcons(): void {
+  ClarityIcons.addIcons(timelineIcon);
+}
