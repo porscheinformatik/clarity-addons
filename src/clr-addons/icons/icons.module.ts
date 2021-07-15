cbdd50e2cf3c3dfb43cb0c20bdb222b197ee3af6
Szilard Veress <Szilard.Veress@cloudflight.io>
2021-07-15 13:25:44 +0300

    Upgrade version

--- src/clr-addons/icons/icons.module.ts
+++ src/clr-addons/icons/icons.module.ts
@@ -11,6 +11,7 @@ import { ClarityModule } from '@clr/angular';
 import { ClarityIcons } from '@clr/icons';
 import { AllShapes } from '@clr/icons/shapes/all-shapes';
 import { ClrAddonsIconShapes } from './shapes';
+import { initCdsIcons } from './cds-icons';
 
 /**
  * Adds custom icons to the ClarityIcons Api
@@ -23,6 +24,8 @@ export function loadIcons() {
     ClarityIcons.add(AllShapes);
     // Add our custom shapes
     ClarityIcons.add(ClrAddonsIconShapes);
+    // Initialize CDS icons
+    initCdsIcons();
   };
 }
 
