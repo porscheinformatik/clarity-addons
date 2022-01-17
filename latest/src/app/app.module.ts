f044b9a8ac5211f43d796093788496b623ba2070
Marcel Ring <33118495+Wallabeng@users.noreply.github.com>
2022-01-17 15:19:36 +0100

    [WEB] Update angular and clarity to v12 (#1453)
    Co-authored-by: Marcel Ring (windm) <extern.marcel.winder@porscheinformatik.at>

--- latest/src/app/app.module.ts
+++ latest/src/app/app.module.ts
@@ -1,17 +1,24 @@
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {BrowserModule, Title} from "@angular/platform-browser";
-import {NgModule} from "@angular/core";
-import {ClarityModule} from "@clr/angular";
-import {ClrAddonsModule} from '@porscheinformatik/clr-addons';
+import { APP_INITIALIZER, NgModule } from "@angular/core";
+import { BrowserModule, Title } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ClarityModule } from "@clr/angular";
+import { ClarityIcons } from "@clr/icons";
+import { AllShapes } from '@clr/icons/shapes/all-shapes';
+import { ClrAddonsModule, ClrAddonsIconShapes } from '@porscheinformatik/clr-addons';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { FullPageLayoutsRoutingModule } from "./documentation/demos/full-page-layouts/full-page-layouts-routing.module";
+import { DocumentationModule } from "./documentation/documentation.module";
+import { HomeComponent } from "./home/home.component";
+import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 
-import {AppRoutingModule} from "./app-routing.module";
-import {AppComponent} from "./app.component";
-import {HomeComponent} from "./home/home.component";
-import {PageNotFoundComponent} from "./page-not-found/page-not-found.component";
-
-import {DocumentationModule} from "./documentation/documentation.module";
-
-import {FullPageLayoutsRoutingModule} from "./documentation/demos/full-page-layouts/full-page-layouts-routing.module"
+export function loadIcons() {
+  return (): void => {
+    // All shapes are added currently, could be reduced to only the shape categories needed
+    ClarityIcons.add(AllShapes);
+    ClarityIcons.add(ClrAddonsIconShapes);
+  };
+}
 
 @NgModule({
     declarations: [
@@ -30,6 +37,11 @@ import {FullPageLayoutsRoutingModule} from "./documentation/demos/full-page-layo
     ],
     providers: [
         Title,
+        {
+          provide: APP_INITIALIZER,
+          useFactory: loadIcons,
+          multi: true,
+        }
     ],
     bootstrap: [AppComponent]
 })
--- latest/src/app/icons/icons.module.ts
+++ latest/src/app/icons/icons.module.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from "@angular/common";
 import { NgModule } from "@angular/core";
 import { ClarityModule } from "@clr/angular";
+import { UtilsModule } from "../utils/utils.module";
 import { IconsGetStartedComponent } from "./icons-get-started/icons-get-started.component";
 import { IconsRoutingModule } from "./icons-routing.module";
 import { IconsSetsComponent } from "./icons-sets/icons-sets.component";
@@ -15,7 +16,8 @@ import { IconsComponent } from "./icons.component";
     imports: [
         CommonModule,
         ClarityModule,
-        IconsRoutingModule
+        IconsRoutingModule,
+        UtilsModule
     ],
     providers: []
 })
