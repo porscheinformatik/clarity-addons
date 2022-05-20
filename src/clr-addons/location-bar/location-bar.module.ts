dfc545e1b3bb7ee604f07774111ff4482857917e
Daniel Deusing <danieldeusing@users.noreply.github.com>
2022-05-20 05:36:03 +0200

    add import of dropdownoverflow for modules which implement dropdowns (#1671)
    Co-authored-by: Daniel Deusing <daniel.deusing@twiced.de>

--- src/clr-addons/location-bar/location-bar.module.ts
+++ src/clr-addons/location-bar/location-bar.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { ClrDropdownModule, ClrIconModule } from '@clr/angular';
 import { LocationBarComponent } from './location-bar.component';
 import { LocationBarNodeComponent } from './location-bar-node/location-bar-node.component';
+import { ClrDropdownOverflowModule } from '../dropdown';
 
 @NgModule({
   declarations: [LocationBarComponent, LocationBarNodeComponent],
-  imports: [CommonModule, ClrIconModule, ClrDropdownModule],
+  imports: [CommonModule, ClrIconModule, ClrDropdownModule, ClrDropdownOverflowModule],
   exports: [LocationBarComponent],
 })
 export class ClrLocationBarModule {}
--- src/clr-addons/multilingual/multilingual.module.ts
+++ src/clr-addons/multilingual/multilingual.module.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2021 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2022 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -12,9 +12,10 @@ import { ClrRequiredAllMultilang, ClrRequiredOneMultilang } from './multilingual
 import { ClrMultilingualInput } from './multilingual-input/multilingual-input';
 import { ClrMultilingualTextarea } from './multilingual-textarea/multilingual-textarea';
 import { ClrMultilingualSelector } from './multilingual-selector';
+import { ClrDropdownOverflowModule } from '../dropdown';
 
 @NgModule({
-  imports: [CommonModule, ClarityModule, FormsModule],
+  imports: [CommonModule, ClarityModule, FormsModule, ClrDropdownOverflowModule],
   declarations: [
     ClrMultilingualInput,
     ClrMultilingualTextarea,
