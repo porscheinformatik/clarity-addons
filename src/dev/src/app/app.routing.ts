c29dec3c44d07d1883c9615766d225c107ab5344
Jeroen van Warmerdam <jeronevw@hotmail.com>
2023-03-02 14:26:55 +0100

    Demo

--- src/dev/src/app/app.routing.ts
+++ src/dev/src/app/app.routing.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2022 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2023 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -130,6 +130,10 @@ export const APP_ROUTES: Routes = [
   {
     path: 'enum-filter',
     loadChildren: () => import('./enum-filter/enum-filter.demo.module').then(m => m.EnumFilterDemoModule),
+  },
+  {
+    path: 'daterangepicker',
+    loadChildren: () => import('./daterangepicker/daterangepicker.demo.module').then(m => m.DaterangepickerDemoModule),
   },
 ];
 
---
+++ src/dev/src/app/daterangepicker/daterangepicker.demo.ts
@@ -0,0 +1,95 @@
+import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { DaterangePreset, DayModel, NullableDaterange } from '../../../../clr-addons/daterangepicker';
+
+@Component({
+  selector: 'clr-daterangepicker-demo',
+  templateUrl: './daterangepicker.demo.html',
+  styleUrls: ['./daterangepicker.demo.scss'],
+})
+export class DaterangepickerDemo {
+  reactiveForm = new FormGroup({
+    daterange: new FormControl(null),
+    otherValue: new FormControl(''),
+  });
+
+  templateDrivenValue: NullableDaterange = null;
+
+  validationForm = new FormGroup({
+    daterange: new FormControl(null, {
+      validators: [Validators.required],
+    }),
+  });
+  minDate = new Date(2023, 0, 1);
+  maxDate = new Date(2024, 11, 31);
+
+  configurationValue: NullableDaterange = null;
+
+  presets = DATERANGEPICKER_PRESETS;
+
+  presetsI18n: Array<DaterangePreset> = [
+    {
+      text: 'Laatste 30 days',
+      range: () => ({
+        from: new DayModel(new Date()).incrementBy(-29),
+        to: new DayModel(new Date()),
+      }),
+    },
+  ];
+
+  public updateReactiveForm(): void {
+    this.reactiveForm.patchValue({
+      daterange: {
+        from: new DayModel(new Date()),
+        to: new DayModel(new Date()).incrementBy(7),
+      },
+    });
+  }
+
+  public updateTemplateDriven(): void {
+    this.templateDrivenValue = {
+      from: new DayModel(new Date()),
+      to: new DayModel(new Date()).incrementBy(7),
+    };
+  }
+
+  public triggerMinMaxValidation(): void {
+    this.validationForm.patchValue({
+      daterange: {
+        from: new DayModel(2020, 4, 3),
+        to: new DayModel(2030, 4, 3),
+      },
+    });
+  }
+}
+
+const DATERANGEPICKER_PRESETS: Array<DaterangePreset> = [
+  {
+    text: 'Last 30 days',
+    range: () => ({
+      from: new DayModel(new Date()).incrementBy(-29),
+      to: new DayModel(new Date()),
+    }),
+  },
+  {
+    text: 'From today',
+    range: () => ({
+      from: new DayModel(new Date()),
+      to: null,
+    }),
+  },
+  {
+    text: 'Until today',
+    range: () => ({
+      from: null,
+      to: new DayModel(new Date()),
+    }),
+  },
+  {
+    text: 'Clear',
+    range: () => ({
+      from: null,
+      to: null,
+    }),
+  },
+];
