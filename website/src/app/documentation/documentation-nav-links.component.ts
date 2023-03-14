f00fff42f30f35240158130ae7094ec9d30fe01f
Martin Brom <martinbrom97@gmail.com>
2023-03-14 12:44:58 +0100

    Replace sidenav with vertical-nav for demo pages

--- website/src/app/documentation/documentation-nav-links.component.ts
+++ website/src/app/documentation/documentation-nav-links.component.ts
@@ -6,13 +6,17 @@ const COMPONENTS = require('../../settings/componentlist.json');
   selector: 'documentation-nav-links',
   template: `
     <ng-container *ngFor="let component of components">
-      <li *ngIf="component.url && !component.noDemo && component.type == type">
-        <a class="nav-link" [routerLink]="component.url" routerLinkActive="active">
-          {{ component.text }}
-          <span *ngIf="component.isNew" class="new nav-link-tag">New!</span>
-          <span *ngIf="component.isUpdated" class="updated nav-link-tag">Updated</span>
-        </a>
-      </li>
+      <a
+        *ngIf="component.url && !component.noDemo && component.type == type"
+        clrVerticalNavLink
+        class="nav-link"
+        [routerLink]="component.url"
+        routerLinkActive="active"
+      >
+        {{ component.text }}
+        <span *ngIf="component.isNew" class="new nav-link-tag">New!</span>
+        <span *ngIf="component.isUpdated" class="updated nav-link-tag">Updated</span>
+      </a>
     </ng-container>
   `,
 })
