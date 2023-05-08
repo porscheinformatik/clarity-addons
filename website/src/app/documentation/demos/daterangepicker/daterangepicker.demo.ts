d04cff9d79f87bc86c4cf5469759fbb58e485ad2
Jeroen van Warmerdam <jeronevw@hotmail.com>
2023-05-08 22:43:02 +0200

    More website documentation. WIP

--- website/src/app/documentation/demos/daterangepicker/daterangepicker.demo.ts
+++ website/src/app/documentation/demos/daterangepicker/daterangepicker.demo.ts
@@ -1,15 +1,28 @@
 import { Component } from '@angular/core';
 import { ClarityDocComponent } from '../clarity-doc';
+import { NullableDaterange } from '@porscheinformatik/clr-addons';
 
 const BASIC_DEMO = `
 <form clrForm>
   <clr-daterangepicker-container>
-    <label>Daterange</label>
-    <input clrDaterangepicker type="date" name="daterange" [(ngModel)]="value" />
+    <label>Basic Demo</label>
+    <input clrDaterangepicker type="date" name="demo" [(ngModel)]="demo" />
   </clr-daterangepicker-container>
 </form>
 `;
 const MIN_MAX_DEMO = `
+<form clrForm>
+  <clr-daterangepicker-container>
+    <label>Min date: 2023-05-03 AND Max date: 2023-06-20</label>
+    <input clrDaterangepicker type="date" name="demo" [(ngModel)]="demo" min="2023-05-03" max="2023-06-20" />
+    <clr-control-error *clrIfDaterangeError="'min'; error as err">
+      Daterange minimum is {{ err.min.toDate() | date }}
+    </clr-control-error>
+    <clr-control-error *clrIfDaterangeError="'max'; error as err">
+      Daterange maximum is {{ err.max.toDate() | date }}
+    </clr-control-error>
+  </clr-daterangepicker-container>
+</form>
 `;
 
 @Component({
@@ -21,7 +34,7 @@ const MIN_MAX_DEMO = `
   },
 })
 export class DaterangepickerDemo extends ClarityDocComponent {
-  value: string;
+  value: NullableDaterange;
   basicDemo = BASIC_DEMO;
   minMaxDemo = MIN_MAX_DEMO;
 
