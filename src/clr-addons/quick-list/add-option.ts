b002bdd9fee1035fc334e559d43e05cdec72a421
Daniel Ninaus <daniel.ninaus@smarter-software.com>
2023-08-02 12:55:59 +0200

    add compact mode to quicklist

--- src/clr-addons/quick-list/add-option.ts
+++ src/clr-addons/quick-list/add-option.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2022 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2023 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -19,9 +19,11 @@ export class ClrAddOption<T> {
   @Input() excludedValues: Array<ClrQuickListValue<T>> = [];
   @Input() readonly: string;
   @Input() disabled = false;
+  @Input() showAddButton = false;
 
   @Output() valueChanged = new EventEmitter<ClrQuickListValue<T>>();
   @Output() remove = new EventEmitter<void>();
+  @Output() add = new EventEmitter<void>();
 
   @ViewChild('select', { static: false }) select: ElementRef;
 
