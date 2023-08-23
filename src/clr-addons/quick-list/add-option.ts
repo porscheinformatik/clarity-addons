8a16530994a12d74e72380e274cb62ac02c2328d
Daniel Ninaus <daniel.ninaus@smarter-software.com>
2023-08-23 10:28:40 +0200

    fix for icons for quicklist

--- src/clr-addons/quick-list/add-option.ts
+++ src/clr-addons/quick-list/add-option.ts
@@ -5,9 +5,9 @@
  */
 import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
 import { ClrQuickListValue } from './add-option.service';
-import { ClarityIcons, trashIcon } from '@cds/core/icon';
+import { ClarityIcons, trashIcon, plusIcon } from '@cds/core/icon';
 
-ClarityIcons.addIcons(trashIcon);
+ClarityIcons.addIcons(trashIcon, plusIcon);
 
 @Component({
   selector: 'clr-add-option',
