84ec0b5600aaa27e7d5b6eef956d93174d6d72f6
lefkas95 <38698639+lefkas95@users.noreply.github.com>
2024-01-17 08:14:59 +0100

    Persist hidden state independently (#2054)

--- src/clr-addons/datagrid/datagrid-state-persistence/datagrid-field.directive.ts
+++ src/clr-addons/datagrid/datagrid-state-persistence/datagrid-field.directive.ts
@@ -1,9 +1,15 @@
 import { Directive, Input } from '@angular/core';
 
 @Directive({
-  selector: '[clrDgField]',
+  selector: '[clrDgField],[clrDgFieldKey]',
 })
 export class DatagridFieldDirective {
   @Input()
   clrDgField: string;
+  @Input()
+  clrDgFieldKey: string;
+
+  getFieldName(): string {
+    return this.clrDgField ?? this.clrDgFieldKey;
+  }
 }
