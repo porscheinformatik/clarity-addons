1131df14ec0d5bccb65373b7394eacc881d7895d
alexiapopaa <152892165+alexiapopaa@users.noreply.github.com>
2024-04-12 13:24:51 +0200

    WCP Logo added (#2161)

--- website/src/app/icons/icons.module.ts
+++ website/src/app/icons/icons.module.ts
@@ -6,9 +6,10 @@ import { IconsGetStartedComponent } from './icons-get-started/icons-get-started.
 import { IconsRoutingModule } from './icons-routing.module';
 import { IconsSetsComponent } from './icons-sets/icons-sets.component';
 import { IconsComponent } from './icons.component';
+import { IconsLogosComponent } from './icons-logos/icons-logos.component';
 
 @NgModule({
-  declarations: [IconsComponent, IconsSetsComponent, IconsGetStartedComponent],
+  declarations: [IconsComponent, IconsSetsComponent, IconsGetStartedComponent, IconsLogosComponent],
   imports: [CommonModule, ClarityModule, IconsRoutingModule, UtilsModule],
   providers: [],
 })
