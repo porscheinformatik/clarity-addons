4599732440011228fede8249dd07b420f68ebe5a
mrod <michael.rodgers.ext@porscheinformatik.com>
2024-04-22 20:38:47 +0200

    Directive clrIfWarning

--- src/clr-addons/clr-control-warning/if-warning.directive.ts
+++ src/clr-addons/clr-control-warning/if-warning.directive.ts
@@ -7,6 +7,10 @@ export class ClrIfWarning implements AfterViewInit {
   icon = this.renderer.createElement('cds-icon');
   hostElement: Element;
   inputElement: Element;
+  inputGroup: Element;
+  comboboxWrapper: Element;
+  focusIndicator: Element;
+  private textArea: Element;
 
   constructor(
     private container: ViewContainerRef,
@@ -26,7 +30,7 @@ export class ClrIfWarning implements AfterViewInit {
       setTimeout(() => {
         this.hostElement = this.host.nativeElement?.previousElementSibling;
         const wrapper = this.hostElement?.previousElementSibling;
-        this.setInputElement();
+        this.setInputComponents();
         //Radio buttons have no wrapper
         if (!wrapper) {
           this.renderer.insertBefore(this.hostElement?.parentElement, this.icon, this.hostElement);
@@ -46,34 +50,52 @@ export class ClrIfWarning implements AfterViewInit {
     }
   }
 
-  setInputElement() {
+  setInputComponents() {
     if (this.hostElement) {
       const parent = this.renderer.parentNode(this.hostElement);
-      console.log(this.hasFocusIndicator(parent));
-      const container = parent?.querySelector('[class^="clr-"][class$="-wrapper"]');
-
-      this.inputElement = container?.querySelectorAll('input, select')[0];
+      this.comboboxWrapper = this.hasComboBox(parent);
+      this.focusIndicator = this.hasFocusIndicator(parent);
+      this.inputElement = this.hasInput(parent?.querySelector('[class^="clr-"][class$="-wrapper"]'));
+      this.inputGroup = this.hasGroup(parent?.querySelector('[class^="clr-"][class$="-wrapper"]'));
+      this.textArea = this.hasTextArea(parent?.querySelector('[class^="clr-"][class$="-wrapper"]'));
     }
+  }
+
+  hasInput(element: Element) {
+    if (element && element?.querySelectorAll('input, select').length >= 1) {
+      return element?.querySelectorAll('input, select')[0];
+    }
+    return null;
+  }
+
+  hasComboBox(element: Element) {
+    return element?.querySelector('[class*="clr-combobox-wrapper"]');
+  }
+  hasTextArea(element: Element) {
+    return element?.querySelector('[class*="clr-textarea"]');
   }
 
   hasGroup(element: Element) {
-    return element?.querySelector('[class*="-group"]') !== null;
+    return element?.querySelector('[class*="-group"]');
   }
 
   hasFocusIndicator(element: Element) {
-    console.log(element?.querySelector('[class*="clr-focus-indicator"]'));
-    return element?.querySelector('[class*="clr-focus-indicator"]') !== null;
+    return element?.querySelector('[class*="clr-focus-indicator"]');
   }
 
   resetControlStyles() {
-    if (this.inputElement) {
-      this.renderer.removeClass(this.inputElement, 'clr-control-warning');
-    }
+    this.focusIndicator ? this.renderer.removeClass(this.focusIndicator, 'clr-warning') : null;
+    this.comboboxWrapper ? this.renderer.removeClass(this.comboboxWrapper, 'clr-warning') : null;
+    this.inputElement ? this.renderer.removeClass(this.inputElement, 'clr-warning') : null;
+    this.inputGroup ? this.renderer.removeClass(this.inputGroup, 'clr-warning') : null;
+    this.textArea ? this.renderer.removeClass(this.textArea, 'clr-warning') : null;
   }
 
   setControlStyles() {
-    if (this.inputElement) {
-      this.renderer.addClass(this.inputElement, 'clr-control-warning');
-    }
+    this.focusIndicator ? this.renderer.addClass(this.focusIndicator, 'clr-warning') : null;
+    this.comboboxWrapper ? this.renderer.addClass(this.comboboxWrapper, 'clr-warning') : null;
+    this.inputElement ? this.renderer.addClass(this.inputElement, 'clr-warning') : null;
+    this.inputGroup ? this.renderer.addClass(this.inputGroup, 'clr-warning') : null;
+    this.textArea ? this.renderer.addClass(this.textArea, 'clr-warning') : null;
   }
 }
