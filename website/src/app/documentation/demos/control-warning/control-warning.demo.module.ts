6d79f540beb1ad828170cf3a270c335959561711
michaelrodgers <michael.rodgers.ext@porscheinformatik.com>
2024-04-30 14:20:05 +0200

    Directive clrIfWarning

--- website/src/app/documentation/demos/control-warning/control-warning.demo.module.ts
+++ website/src/app/documentation/demos/control-warning/control-warning.demo.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ControlWarningDemo } from './control-warning.demo';
-import { ClrAddonsModule } from 'src/clr-addons';
+import { ClrAddonsModule } from '@porscheinformatik/clr-addons';
 import { ClarityModule } from '@clr/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
