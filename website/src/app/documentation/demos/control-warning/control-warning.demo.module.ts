a79c545b7daa566da02da2a3368ffd9bf76d6787
gc62767 <167352274+gc62767@users.noreply.github.com>
2024-05-07 06:32:01 +0200

    if-warning Directive for clr-helper-components (#2176) (#2181)
    (cherry picked from commit 39a636e123dcd96cbcd2f9f79493f98aea7d361a)

---
+++ website/src/app/documentation/demos/control-warning/control-warning.demo.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+import { ControlWarningDemo } from './control-warning.demo';
+import { ClrAddonsModule } from '@porscheinformatik/clr-addons';
+import { ClarityModule } from '@clr/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [ControlWarningDemo],
+  imports: [
+    CommonModule,
+    ClrAddonsModule,
+    RouterModule.forChild([{ path: '', component: ControlWarningDemo }]),
+    ClarityModule,
+    FormsModule,
+    ReactiveFormsModule,
+  ],
+})
+export class ControlWarningDemoModule {}
