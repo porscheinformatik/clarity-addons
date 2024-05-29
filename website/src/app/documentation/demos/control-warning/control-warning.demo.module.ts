6b8132070e1e6492bce0f8003c33d9ab98813fd3
gc62767 <167352274+gc62767@users.noreply.github.com>
2024-05-29 14:06:53 +0200

    Website documentation for clr-control-warning (#2204)

--- website/src/app/documentation/demos/control-warning/control-warning.demo.module.ts
+++ website/src/app/documentation/demos/control-warning/control-warning.demo.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ControlWarningDemo } from './control-warning.demo';
 import { ClrAddonsModule } from '@porscheinformatik/clr-addons';
 import { ClarityModule } from '@clr/angular';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { DocWrapperModule } from '../_doc-wrapper/doc-wrapper.module';
+import { UtilsModule } from '../../../utils/utils.module';
 
 @NgModule({
   declarations: [ControlWarningDemo],
@@ -14,7 +16,11 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     RouterModule.forChild([{ path: '', component: ControlWarningDemo }]),
     ClarityModule,
     FormsModule,
+    DocWrapperModule,
     ReactiveFormsModule,
+    UtilsModule,
+    AsyncPipe,
   ],
+  exports: [ControlWarningDemo],
 })
 export class ControlWarningDemoModule {}
