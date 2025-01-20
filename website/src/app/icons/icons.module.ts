57fc07b2406ab1b110f502238e9b1378bdb97f62
Daniel Ninaus <daniel.ninaus@smarter-software.com>
2025-01-20 13:30:49 +0100

    reorganize icons, added d6icons, logos and poi custom icons

--- website/src/app/icons/icons.module.ts
+++ website/src/app/icons/icons.module.ts
@@ -1,16 +1,26 @@
-import { CommonModule } from '@angular/common';
+import { CommonModule, NgTemplateOutlet } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ClarityModule } from '@clr/angular';
+import { ClarityModule, ClrIconModule } from '@clr/angular';
 import { UtilsModule } from '../utils/utils.module';
 import { IconsGetStartedComponent } from './icons-get-started/icons-get-started.component';
 import { IconsRoutingModule } from './icons-routing.module';
 import { IconsSetsComponent } from './icons-sets/icons-sets.component';
 import { IconsComponent } from './icons.component';
 import { IconsLogosComponent } from './icons-logos/icons-logos.component';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [IconsComponent, IconsSetsComponent, IconsGetStartedComponent, IconsLogosComponent],
-  imports: [CommonModule, ClarityModule, IconsRoutingModule, UtilsModule],
+  imports: [
+    CommonModule,
+    ClarityModule,
+    IconsRoutingModule,
+    UtilsModule,
+    FormsModule,
+    ClrIconModule,
+    NgTemplateOutlet,
+    NgTemplateOutlet,
+  ],
   providers: [],
 })
 export class IconsModule {}
