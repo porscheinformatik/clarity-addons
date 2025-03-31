porscheinformatik/clarity-addons

commit 29bddf43556010bb2676eabfdc7f935971f63434

Upgrade Clarity monorepo to 17.6.0 & Fix combobox content cut off (#2466)

src/clr-addons/themes/phs/_css.overrides.scss
@@ -240,7 +240,8 @@
   }
 
   .clr-combobox-wrapper,
-  .clr-combobox-input {
+  .clr-combobox-input,
+  .clr-combobox-input-wrapper {
     width: 100%;
   }
 

commit 2908213b4427ae36fe984f82e250b6eb08222f20

Fix issue caused by 29bddf43 (default combobox text input has wrong width)

src/clr-addons/themes/phs/_css.overrides.scss
@@ -240,8 +240,16 @@
   }
 
   .clr-combobox-wrapper,
-  .clr-combobox-input,
-  .clr-combobox-input-wrapper {
+  .clr-combobox-input {
+    width: 100%;
+  }
+
+  // Combobox input wrapper is doing two different things:
+  // - in single selection mode, then it's just like a text input
+  // - in multi selection mode, it's a text input NEXT TO the selected items
+  // If we don't set 100% in single selection mode, the input could be too small and cut off the selected value.
+  // But if we set 100% also in multi selection mode, then we mess up the flex layout of the two columns.
+  .clr-combobox-input-wrapper:not(:has(.label-combobox-pill)) {
     width: 100%;
   }
 

src/dev/src/app/app.routing.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2024 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2025 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -19,6 +19,7 @@ export const APP_ROUTES: Routes = [
     loadChildren: () => import('./back-button/back-button.demo.module').then(m => m.BackButtonDemoModule),
   },
   { path: 'cards', loadChildren: () => import('./cards/cards.demo.module').then(m => m.CardsDemoModule) },
+  { path: 'combobox', loadComponent: () => import('./combobox/combobox.demo').then(m => m.ComboboxDemo) },
   {
     path: 'view-edit-section',
     loadChildren: () =>

src/dev/src/app/combobox/combobox.demo.html
@@ -1,0 +1,49 @@
+<h4>Combobox Single Selection</h4>
+<div class="clr-row">
+  <div class="clr-col-6">
+    <clr-combobox-container>
+      <label>ComboBox SingleSelect</label>
+      <clr-combobox [(ngModel)]="singleSelection" name="singleSelect">
+        <ng-container *clrOptionSelected="let selected"> {{ selected }}</ng-container>
+        <clr-options>
+          <clr-option *clrOptionItems="let item of items" [clrValue]="item"> {{ item }} </clr-option>
+        </clr-options>
+      </clr-combobox>
+    </clr-combobox-container>
+  </div>
+</div>
+
+<h4>Combobox Multi Selection - Default</h4>
+<div class="clr-row">
+  <div class="clr-col-6">
+    <clr-combobox-container>
+      <label>ComboBox MultiSelect</label>
+      <clr-combobox [(ngModel)]="multiSelection" name="multiSelect" clrMulti="true">
+        <ng-container *clrOptionSelected="let selected"> {{ selected }}</ng-container>
+        <clr-options>
+          <clr-option *clrOptionItems="let item of items" [clrValue]="item"> {{ item }} </clr-option>
+        </clr-options>
+      </clr-combobox>
+    </clr-combobox-container>
+  </div>
+</div>
+
+<h4>Combobox Multi Selection - Vertical</h4>
+<div class="clr-row">
+  <div class="clr-col-6">
+    <clr-combobox-container>
+      <label>ComboBox MultiSelect Vertical</label>
+      <clr-combobox
+        [(ngModel)]="multiSelectionVertical"
+        name="multiSelectVertical"
+        clrMulti="true"
+        class="clr-combobox-vertical"
+      >
+        <ng-container *clrOptionSelected="let selected"> {{ selected }}</ng-container>
+        <clr-options>
+          <clr-option *clrOptionItems="let item of items" [clrValue]="item"> {{ item }} </clr-option>
+        </clr-options>
+      </clr-combobox>
+    </clr-combobox-container>
+  </div>
+</div>

src/dev/src/app/combobox/combobox.demo.ts
@@ -1,0 +1,26 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ClarityModule, ClrComboboxModule } from '@clr/angular';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClrFormModule } from '@porscheinformatik/clr-addons';
+
+@Component({
+  selector: 'clr-combobox-demo',
+  templateUrl: './combobox.demo.html',
+  imports: [CommonModule, ClarityModule, FormsModule, ReactiveFormsModule, ClrComboboxModule, ClrFormModule],
+})
+export class ComboboxDemo {
+  public items: string[] = [
+    'Item 1',
+    'Item 2 is very very very very very very very very very very very very very very very very very long',
+    'Item 3',
+    'Item 4',
+    'Item 5',
+    'Item 6 is also very very very very very very very very very very long',
+    'Item 7',
+  ];
+
+  public singleSelection: string;
+  public multiSelection: string[] = [];
+  public multiSelectionVertical: string[] = [];
+}