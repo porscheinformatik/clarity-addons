59d0fc718de9cd150eca8a74dbbdba221bb38db0
Tim Wagner <tim.wagner@porscheinformatik.com>
2025-07-22 09:34:24 +0200

    Extend treetable with sorting and multi selection functionality (#2580)

---
+++ src/clr-addons/treetable/providers/index.ts
@@ -0,0 +1,3 @@
+export * from './selection';
+export * from './sort';
+export * from './items';
--- src/clr-addons/treetable/treetable-row.spec.ts
+++ src/clr-addons/treetable/treetable-row.spec.ts
@@ -7,10 +7,13 @@
 import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { ClarityModule } from '@clr/angular';
 import { FormsModule } from '@angular/forms';
-import { ClrTreetableModule, ClrTreetableRow } from '@porscheinformatik/clr-addons';
-import { Component, ViewChild } from '@angular/core';
+import { ClrTreetableModule, ClrTreetableRow, Items, Selection, Sort } from '@porscheinformatik/clr-addons';
+import { Component, QueryList, ViewChild, ViewChildren } from '@angular/core';
 import { By } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { SelectionType } from './enums/selection-type';
+
+type Item = { id: number };
 
 @Component({
   template: `
@@ -27,7 +30,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   standalone: false,
 })
 class RowClickableTestComponent {
-  @ViewChild(ClrTreetableRow, { static: true }) ttRow: ClrTreetableRow;
+  @ViewChild(ClrTreetableRow, { static: true }) ttRow: ClrTreetableRow<Item>;
+}
+
+@Component({
+  template: `
+    <clr-treetable [(clrTtSelected)]="selected">
+      <clr-tt-row *clrTtItems="allItems; let item" [clrTtItem]="item">
+        <clr-tt-cell></clr-tt-cell>
+      </clr-tt-row>
+    </clr-treetable>
+  `,
+  standalone: false,
+})
+class SelectableTestComponent {
+  @ViewChildren(ClrTreetableRow) ttRows: QueryList<ClrTreetableRow<Item>>;
+
+  allItems: Item[] = [{ id: 1 }, { id: 2 }, { id: 3 }];
+  selected: Item[] = [this.allItems[0]];
 }
 
 @Component({
@@ -39,20 +59,25 @@ class RowClickableTestComponent {
   standalone: false,
 })
 class EmptyTestComponent {
-  @ViewChild(ClrTreetableRow, { static: true }) ttRow: ClrTreetableRow;
+  @ViewChild(ClrTreetableRow, { static: true }) ttRow: ClrTreetableRow<Item>;
 }
 
 describe('ClrTreetableRow', () => {
   let emptyTestComponent: EmptyTestComponent;
   let rowClickableTestComponent: RowClickableTestComponent;
+  let selectableTestComponent: SelectableTestComponent;
 
   let rowClickableTestComponentFixture: ComponentFixture<RowClickableTestComponent>;
   let emptyTestComponentFixture: ComponentFixture<EmptyTestComponent>;
+  let selectableTestComponentFixture: ComponentFixture<SelectableTestComponent>;
+
+  let selection: Selection<Item>;
 
   beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
-      declarations: [EmptyTestComponent, RowClickableTestComponent],
+      declarations: [EmptyTestComponent, RowClickableTestComponent, SelectableTestComponent],
       imports: [ClarityModule, FormsModule, ClrTreetableModule, BrowserAnimationsModule],
+      providers: [Selection, Items, Sort],
       teardown: { destroyAfterEach: false },
     }).compileComponents();
 
@@ -63,6 +88,11 @@ describe('ClrTreetableRow', () => {
     emptyTestComponentFixture = TestBed.createComponent(EmptyTestComponent);
     emptyTestComponent = emptyTestComponentFixture.componentInstance;
     emptyTestComponentFixture.detectChanges();
+
+    selectableTestComponentFixture = TestBed.createComponent(SelectableTestComponent);
+    selectableTestComponent = selectableTestComponentFixture.componentInstance;
+    selectableTestComponentFixture.detectChanges();
+    selection = TestBed.inject(Selection);
   }));
 
   it('should create', () => {
@@ -75,5 +105,30 @@ describe('ClrTreetableRow', () => {
     row.triggerEventHandler('click', { target: row.nativeElement });
     rowClickableTestComponentFixture.detectChanges();
     expect(rowClickableTestComponent.ttRow.expanded).toBeTruthy();
+  });
+
+  it('should preselect given item', () => {
+    selection.selectionType = SelectionType.Multi;
+
+    const rows = selectableTestComponent.ttRows.toArray();
+
+    expect(rows[0].selected).toBe(true);
+    expect(rows[1].selected).toBe(false);
+    expect(rows[2].selected).toBe(false);
+  });
+
+  it('should toggle selection for a row', () => {
+    selection.selectionType = SelectionType.Multi;
+
+    const firstRow = selectableTestComponent.ttRows.toArray()[0];
+    const selectedChangedSpy = spyOn(firstRow.selectedChanged, 'emit');
+
+    firstRow.toggle();
+    expect(firstRow.selected).toBe(false);
+    expect(selectedChangedSpy).toHaveBeenCalledWith(false);
+
+    firstRow.toggle();
+    expect(firstRow.selected).toBe(true);
+    expect(selectedChangedSpy).toHaveBeenCalledWith(true);
   });
 });
