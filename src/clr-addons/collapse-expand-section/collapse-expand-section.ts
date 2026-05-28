cb5ee922d8386d2a572220215b56439c9d3be138
markofilipovicext <marko.filipovic.ext@porscheinformatik.com>
2026-05-28 10:53:30 +0200

    Collapse/Expand & View/Edit: collapsed chevron pointing right + color (#2869)
    - Adapt collapse-expand and view-edit components to display collapsed chevron pointing right and with icon color.

--- src/clr-addons/collapse-expand-section/collapse-expand-section.ts
+++ src/clr-addons/collapse-expand-section/collapse-expand-section.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2025 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2026 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -25,7 +25,7 @@ ClarityIcons.addIcons(angleIcon);
       ]),
     ]),
     trigger('rotateIcon', [
-      state('true', style({ transform: 'rotate(0)' })),
+      state('true', style({ transform: 'rotate(90deg)' })),
       state('false', style({ transform: 'rotate(180deg)' })),
       transition('true => false', animate('300ms ease-out')),
       transition('false => true', animate('300ms ease-in')),
