8005a26034f129062bd1331c8be666d45b0ae890
Marcel Ring <33118495+Wallabeng@users.noreply.github.com>
2026-06-01 09:26:48 +0200

    Fix versions displayed in docu (#2877)

--- website/src/environments/environment.prod.ts
+++ website/src/environments/environment.prod.ts
@@ -1,9 +1,9 @@
 export const environment = {
   production: true,
   version: 'latest',
-  addons_version: '19.21.0',
-  angular_version: '21.2.13',
-  clarity_version: '18.1.0',
+  addons_version: '@ADDONS_VERSION',
+  angular_version: '@ANGULAR_VERSION',
+  clarity_version: '@CLARITY_VERSION',
   versions_url: '/clarity-addons/versions.json',
   base: '/clarity-addons',
 };
