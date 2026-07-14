e5e48cce014413bb7c431aa7dc572f9bbc610824
Lukas Rastinger <lukas.rastinger@porscheinformatik.com>
2026-07-14 11:14:10 +0200

    Fixes treetable header background color and treetable row selection alignment.

--- src/clr-addons/treetable/treetable-cell.ts
+++ src/clr-addons/treetable/treetable-cell.ts
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2018-2025 Porsche Informatik. All Rights Reserved.
+ * Copyright (c) 2018-2026 Porsche Informatik. All Rights Reserved.
  * This software is released under MIT license.
  * The full license information can be found in LICENSE in the root directory of this project.
  */
@@ -11,7 +11,7 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   template: '<ng-content></ng-content>',
   host: {
     '[class.treetable-cell]': 'true',
-    role: 'cell',
+    role: 'gridcell',
   },
   standalone: false,
   changeDetection: ChangeDetectionStrategy.OnPush,
